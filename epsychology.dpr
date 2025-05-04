5663b36c3effb211f34467a7a4fa1df7dd284311
Lucas de Paula Monti <155757043+lucaspaulamonti@users.noreply.github.com>
2025-05-04 23:54:48 +0000

    gitignore

--- epsychology.dpr
+++ epsychology.dpr
@@ -2,13 +2,21 @@ program epsychology;
 
 uses
   Vcl.Forms,
-  UnitAttributeCadastre in 'Source\Module\Attribute\Form\Delphi 12\VCL\UnitAttributeCadastre.pas' {Form1};
+  UnitCadastre in 'Source\Module\Attribute\Form\Delphi 12\VCL\UnitCadastre.pas' {FormCadastre},
+  UnitCustomer in 'Source\Module\Customer\Form\Delphi 12\VCL\UnitCustomer.pas' {FormCustomer},
+  UnitUser in 'Source\Module\User\Form\Delphi 12\VCL\UnitUser.pas' {FormUser},
+  UnitMedical in 'Source\Module\Medical\Form\Delphi 12\VCL\UnitMedical.pas' {FormMedical},
+  UnitInstitutional in 'Source\Module\Institutional\Form\Delphi 12\VCL\UnitInstitutional.pas' {FormInstitutional};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormCadastre, FormCadastre);
+  Application.CreateForm(TFormCustomer, FormCustomer);
+  Application.CreateForm(TFormUser, FormUser);
+  Application.CreateForm(TFormMedical, FormMedical);
+  Application.CreateForm(TFormInstitutional, FormInstitutional);
   Application.Run;
 end.
