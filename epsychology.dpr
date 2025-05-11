9cc51ebac12912cdc7413d9bb0a5f815d898cf57
Lucas de Paula Monti <155757043+lucaspaulamonti@users.noreply.github.com>
2025-05-11 14:14:12 +0000

    gitignore

--- epsychology.dpr
+++ epsychology.dpr
@@ -7,7 +7,8 @@ uses
   UnitUser in 'Source\Module\User\Form\Delphi 12\VCL\UnitUser.pas' {FormUser},
   UnitMedical in 'Source\Module\Medical\Form\Delphi 12\VCL\UnitMedical.pas' {FormMedical},
   UnitInstitutional in 'Source\Module\Institutional\Form\Delphi 12\VCL\UnitInstitutional.pas' {FormInstitutional},
-  UnitMain in 'Source\Module\Main\Form\Delphi 12\VCL\UnitMain.pas' {FormMain};
+  UnitMain in 'Source\Module\Main\Form\Delphi 12\VCL\UnitMain.pas' {FormMain},
+  UnitAgenda in 'Source\Module\Agenda\Form\Delphi 12\VCL\UnitAgenda.pas' {FormAgenda};
 
 {$R *.res}
 
@@ -20,6 +21,13 @@ begin
   //Application.CreateForm(TFormUser, FormUser);
   //Application.CreateForm(TFormMedical, FormMedical);
   //Application.CreateForm(TFormInstitutional, FormInstitutional);
+  //Application.CreateForm(TFormAgenda, FormAgenda);
+
+
+
+
+
+
   Application.CreateForm(TFormMain, FormMain);
 
   Application.Run;
